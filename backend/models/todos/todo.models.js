29b1c13e4e9f4e2889b6fb2c659fafaec3656fcd
satyam-kalihari <satyamkalihari17@gmail.com>
2024-07-22 22:11:37 +0530

    Todo data modeling.

--- backend/models/todos/todo.models.js
+++ backend/models/todos/todo.models.js
@@ -1,5 +1,5 @@
 import mongoose, { mongo } from "mongoose";
-import { User } from "./user.models";
+import { User } from "../user.models";
 
 const todoSchema = new mongoose.Schema({
     content : {
